44dfc523ab386d501a660755bf1a60b09d1098c5
Chance Strickland <hi@chance.dev>
2022-06-14 17:49:00 -0700

    tweak home route

--- exercise/01-remix-intro/app/routes/index.jsx
+++ exercise/01-remix-intro/app/routes/index.jsx
@@ -1,5 +1,17 @@
 import * as React from "react";
 
-export default function ProjectsRoute() {
-  return <h1>Hello! Let's build an app 🚀</h1>;
+export default function HomeRoute() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "2rem",
+      }}
+    >
+      <h1>Hello! Let's build an app 🚀</h1>
+    </main>
+  );
 }
